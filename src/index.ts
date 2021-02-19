463e96214e5b3303811e10a8d96d91a4e50acd79
Tyler Eisenhauer <tylereisenhauer7372@gmail.com>
2021-02-19 13:44:03 -0600

    Add docker stuff

--- src/index.ts
+++ src/index.ts
@@ -15,5 +15,8 @@ app.use(express.json())
 app.use('/api/users', usersRouter)
 app.use('/api/sales', auth.enforceRole('admin'), salesRouter)
 app.use('/api/raffle', auth.enforceRole('admin'), raffleRouter)
+app.use('/api/', (req, res) => {
+    res.send('NukeBot API')
+})
 
 app.listen(3000)
